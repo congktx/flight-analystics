2d348381dfd5ad71ce85fea1d3aea894df9fa3e5
Loi-ND <loinguyenduy12a1@gmail.com>
2025-11-08 20:25:56 +0700

    update company_crawler

--- src/crawler/company_crawler.py
+++ src/crawler/company_crawler.py
@@ -4,6 +4,7 @@ from utils.config import *
 from utils.utils import save_info
 from pprint import pprint
 import time
+from typing import List, Dict
 
 def get_company_infos(date: str, exchange: str):
     url = "https://api.massive.com/v3/reference/tickers"
@@ -26,17 +27,19 @@ def get_company_infos(date: str, exchange: str):
     try:
         res = requests.get(url=url, params=params, headers=headers).json()
         results = res["results"]
+        results = adjust_data(data=results,
+                              date=date)
         next_url = res.get("next_url")
         save_info(path=GlobalConfig.COMPANY_DATA_PATH,
                   data=results,
                   id=1)
         if next_url:
             cursor = next_url.split("cursor=")[-1]
-            get_next_infos(cursor=cursor, id=2)
+            get_next_infos(cursor=cursor, date=date, id=2)
     except Exception as e:
         print(f"Error while fetching data: {e} -> {1}")
 
-def get_next_infos(cursor: str, id: int):
+def get_next_infos(cursor: str, date: str, id: int):
     url = "https://api.massive.com/v3/reference/tickers"
 
     headers = {
@@ -53,6 +56,7 @@ def get_next_infos(cursor: str, id: int):
             params["cursor"] = cursor
             res = requests.get(params=params, headers=headers, url=url).json()
             results = res["results"]
+            adjust_data(data=results, date=date)
             next_url = res.get("next_url")
             save_info(path=GlobalConfig.COMPANY_DATA_PATH,
                       data=results,
@@ -64,3 +68,10 @@ def get_next_infos(cursor: str, id: int):
                 cursor=None
     except Exception as e:
         print(f"Error while fetching data: {e} -> {id}")
+
+def adjust_data(data: List[Dict], date: str):
+    results = []
+    for inf in data:
+        inf["update_time"] = date
+        results.append(inf)
+    return results
