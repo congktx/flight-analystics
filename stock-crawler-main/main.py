4a67099e53dc077cba0d3a2a0720db8ec399413c
maithinhft <maithinh.lhpnd@gmail.com>
2025-11-12 19:54:09 +0700

    fix: crawl next api

--- stock-crawler-main/main.py
+++ stock-crawler-main/main.py
@@ -4,6 +4,13 @@ from tqdm import tqdm
 from service import *
 
 if __name__ == '__main__':
-    # crawl_all_company(list_exchage=['XNAS', 'XNYS'])
-    # crawl_news_sentiment(1704067200, 1761969690, 1704067200)
-    crawl_all_ohlc(1704067200, 1761969690, 1704067200)
+    while True:
+        try:
+            # crawl_all_company(list_exchage=['XNAS', 'XNYS'])
+            # crawl_news_sentiment(1704067200, 1761969690, 1704067200)
+            crawl_all_ohlc(1704067200, 1761969690, 1704067200)
+            break
+        except Exception as e:
+            print(e)
+            time.sleep(10)
+    print("Finish")
--- stock-crawler-main/service/ohlc_crawler.py
+++ stock-crawler-main/service/ohlc_crawler.py
@@ -13,11 +13,12 @@ from utils.json_file import load_json_file, save_json_file
 
 mongodb = MongoDB()
 
+
 def get_ohlc(ticker, from_timestamp, to_timestamp):
     from_date = timestamp_to_date(from_timestamp)
     to_date = timestamp_to_date(to_timestamp)
-    url = f'https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/hour/{from_date}/{to_date}'
-    
+    url = f'https://api.massive.com/v2/aggs/ticker/{ticker}/range/1/hour/{from_date}/{to_date}'
+
     headers = {
         "Content-Type": "application/json"
     }
@@ -28,12 +29,12 @@ def get_ohlc(ticker, from_timestamp, to_timestamp):
         "limit": 50000,
         "apiKey": PolygonConfig.API_KEY
     }
-    
+
     try:
         response = requests.get(url, params=params, headers=headers).json()
 
         next_url = response.get('next_url')
-        
+
         if not response.get('results'):
             return response.get('errors'), None
 
@@ -41,21 +42,22 @@ def get_ohlc(ticker, from_timestamp, to_timestamp):
     except Exception as e:
         print(f"Error fetching data: {e}")
         return [], None
-    
+
+
 def ohlc_get_next_url(url):
     headers = {
         "Content-Type": "application/json"
     }
-    
+
     params = {
         "apiKey": PolygonConfig.API_KEY
     }
 
     try:
         response = requests.get(url=url, headers=headers, params=params).json()
-        
+
         next_url = response.get('next_url')
-        
+
         if not response.get('results'):
             return response.get('errors'), None
 
@@ -63,6 +65,7 @@ def ohlc_get_next_url(url):
     except Exception as e:
         print(f"Error fetching data: {e}")
         return [], None
+
 
 def load_all_ohlc_to_db(ticker, list_ohlc, time_update):
     list_documents = []
@@ -80,16 +83,17 @@ def load_all_ohlc_to_db(ticker, list_ohlc, time_update):
         }
 
         list_documents.append(document)
-    
+
     mongodb.upsert_space_many_ohlc(list_documents)
     time.sleep(0.1)
-        
+
+
 def crawl_all_ohlc(from_timestamp, to_timestamp, time_update):
     # timestamp = mongodb.find_last_timestamp(mongodb._company_infos)
     # filter = {
     #     "time_update": timestamp
     # }
-    
+
     # list_company_infos = list(mongodb.find_documets(mongodb._company_infos, filter))
     # tickers = list(map(lambda x: x.get('ticker'), list_company_infos))
     dict = load_json_file('./tmp/division_of_labor.json')
@@ -97,33 +101,32 @@ def crawl_all_ohlc(from_timestamp, to_timestamp, time_update):
 
     last_ticker = mongodb.find_last_timestamp(mongodb._OHLC)
     is_start_crawl = False
-    
-    for ticker in tickers:
-        if last_ticker == 'None' or last_ticker == ticker: is_start_crawl = True
-        
-        if not is_start_crawl: continue
-        
+
+    for index, ticker in enumerate(tickers):
+        if last_ticker == 'None' or last_ticker == ticker:
+            is_start_crawl = True
+
+        if not is_start_crawl:
+            continue
+
         # number_document = mongodb._OHLC.count_documents({
         #     'ticker': ticker
         # })
         # time.sleep(0.1)
-        
+
         # if number_document: continue
-        
-        print(ticker)
+
+        print(index+1, ticker)
 
         mongodb.upsert_last_completed_timestamp(mongodb._OHLC, ticker)
         list_ohlc, next_url = get_ohlc(ticker, from_timestamp, to_timestamp)
         if isinstance(list_ohlc, list):
             load_all_ohlc_to_db(ticker, list_ohlc, time_update)
-        
-        time.sleep(12)
-        
+
+        time.sleep(15)
+
         while next_url:
             list_ohlc, next_url = ohlc_get_next_url(next_url)
             if isinstance(list_ohlc, list):
                 load_all_ohlc_to_db(ticker, list_ohlc, time_update)
-            time.sleep(12)
-        
-        if not isinstance(list_ohlc, list) or len(list_ohlc) == 0:
-            break
+            time.sleep(15)
