367c5cb5ce9570b47e7e16d8cf8acf8960204f8e
longnv <nguyenvietlong9k@gmail.com>
2025-11-18 13:21:55 +0700

    stashed commit

--- stock-crawler-main/main.py
+++ stock-crawler-main/main.py
@@ -5,5 +5,9 @@ from service import *
 
 if __name__ == '__main__':
     # crawl_all_company(list_exchage=['XNYS'])
-    # crawl_news_sentiment(1733011200, 1746057600, 1746057600)
-    crawl_all_ohlc(1733011200, 1745971200, 1745971200)
+    
+    # crawl_assigned_news_sentiment(1704067200, 1761955200, 1761955200)
+
+    # crawl_all_ohlc(1704067200, 1759190400, 1759190400)
+    
+    crawl_assigned_companies_ohlc(1704067200, 1761955200, 1761955200)
